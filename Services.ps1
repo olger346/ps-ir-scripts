385af4b4a42e7336a18a7a63c1c918b61c729510
olger346 <32828582+olger346@users.noreply.github.com>
2021-06-13 21:21:01 -0500

    Add files via upload

---
+++ Services.ps1
@@ -0,0 +1,8 @@
+﻿ $estado = get-service | where-object {$_.Name -eq 'Appinfo'}
+ if ($estado.Status -ne 'Running' ){
+ write-host 'El servicio no se encuentra corriendo'
+ 
+ }elseif ($estado.Status -eq 'Running'){
+ Write-Host 'El servicio se encuentra activo en el equipo'
+ 
+ }
